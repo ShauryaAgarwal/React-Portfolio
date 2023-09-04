7c1c7272188ea3f9614eba060cb9ce9086ce0f1e
Shaurya Agarwal <shaurya220@gmail.com>
2023-09-04 02:30:49 -0400

    Initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://shauryaagarwal.github.io/",
+    "name": "Shaurya Agarwal",
+    "role": "computer science student at Georgia Tech",
+    "linkedinId":"linkedin.com/in/shauryaagarwal1/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am seeking opportunities to apply the technical fields I am passionate about, artificial intelligence and machine learning, in dynamic project environments.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/shauryaagarwal1/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ShauryaAgarwal",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://drive.google.com/file/d/11npC068-OTLGPPg3T7kgNMhiVXNFRW5V/view?usp=sharing",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Versatile undergraduate student able to quickly learn and handle varied responsibilities. Demonstrates a strong work ethic and creative ability. Passion lies in AI and machine learning.",
+    "address":"USA",
+    "website":"https://shauryaagarwal.github.io",
+    "education":[
+      {
+        "UniversityName":"Georgia Institute of Technology",
+        "specialization":"B.S. in Computer Science (Devices & Intelligence), Minor in Business",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2025",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"South Brunswick High School",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2022",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
