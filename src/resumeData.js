42bfd7abaedd2efbd15aa3adc04d3325804347bc
Shaurya Agarwal <shaurya220@gmail.com>
2023-09-04 03:42:49 -0400

    Changed package.json

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,5 +1,5 @@
 let resumeData = {
-    "imagebaseurl":"https://shauryaagarwal.github.io/",
+    "imagebaseurl":"https://shauryaagarwal.github.io/portfolio",
     "name": "Shaurya Agarwal",
     "role": "computer science student at Georgia Tech",
     "linkedinId":"linkedin.com/in/shauryaagarwal1/",
@@ -24,7 +24,7 @@ let resumeData = {
       ],
     "aboutme":"Versatile undergraduate student able to quickly learn and handle varied responsibilities. Demonstrates a strong work ethic and creative ability. Passion lies in AI and machine learning.",
     "address":"USA",
-    "website":"https://shauryaagarwal.github.io",
+    "website":"https://shauryaagarwal.github.io/portfolio",
     "education":[
       {
         "UniversityName":"Georgia Institute of Technology",
@@ -66,7 +66,7 @@ let resumeData = {
         "skillname":"CSS"
       },
       {
-        "skillname":"Reactjs"
+        "skillname":"React.js"
       }
     ],
     "portfolio":[
